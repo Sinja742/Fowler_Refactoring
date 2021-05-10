628562252946cb6e9e06290c3ee022ea304367e8
sinja742 <3pt5grfdYF4>
2021-05-10 16:30:18 +0200

    fixed some code spots so it resembles the code in the book more

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -1,10 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Berkling
- * Date: 14.05.14
- * Time: 12:48
- * To change this template use File | Settings | File Templates.
- */
 public class ChildrensPrice implements Price {
 
 
--- src/Movie.java
+++ src/Movie.java
@@ -2,13 +2,15 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+
     private String title;
     private int priceCode;
     private Price price;
 
-    public Movie(String newtitle, int priceCode) {
-        title = newtitle;
-        setPriceCode(priceCode);
+    public Movie(String title, int priceCode) {
+        this.title = title;
+//        setPriceCode(priceCode);
+        this.priceCode = priceCode;
     }
 
     public int getPriceCode() {
